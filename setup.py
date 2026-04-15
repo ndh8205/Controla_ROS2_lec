0ec5129dca949fbfb802caa890f5efa638d899e6
ndh <ndh98205@@naver.com>
2026-04-15 19:10:33 +0900

    chore: 세미나 실습 범위 외 파일 제거 (570→46)
    비머 슬라이드(4교시 GCO 근접운전)에서 사용하지 않는 노드, 런치, 월드,
    모델, 데이터, 문서를 제거하고 setup.py/package.xml 정리

--- setup.py
+++ setup.py
@@ -23,8 +23,6 @@ data_files_list = package_files('models')
 data_files_list.extend(package_files('worlds'))
 data_files_list.extend(package_files('launch'))
 data_files_list.extend(package_files('data'))
-data_files_list.extend(package_files('config'))
-data_files_list.extend(package_files('urdf'))
 
 # Add package.xml and resource file
 data_files_list.append(('share/ament_index/resource_index/packages', ['resource/' + package_name]))
@@ -46,31 +44,8 @@ setup(
     entry_points={
         'console_scripts': [
             'multi_satellite_controller = orbit_sim.multi_satellite_controller:main',
-            'multi_satellite_controller_service = orbit_sim.multi_satellite_controller_service:main',
-            'multi_satellite_controller_guidence = orbit_sim.multi_satellite_controller_guidence:main',
-            'aruco_detector = orbit_sim.aruco_detector:main',
-            'aruco_simulator = orbit_sim.aruco_simulator:main',
-            'gazebo_aruco_controller = orbit_sim.gazebo_aruco_controller:main',
-            'imu_visualizer = orbit_sim.imu_visualizer:main',
-            'torque_publisher = orbit_sim.torque_publisher_node:main',
             'gco_controller = orbit_sim.gco_controller:main',
-            'force_torque_controller = orbit_sim.force_torque_controller:main',
             'orbit_LVLH_gco = orbit_sim.orbit_LVLH_gco:main',
-            'pose_control_camtest = orbit_sim.pose_control_camtest:main',
-            'lvlh_sim_node = orbit_sim.lvlh_sim_node:main',
-            'lidar_bridge = orbit_sim.lidar_bridge:main',
-            'laser_to_pointcloud = orbit_sim.laser_to_pointcloud:main',
-            'lidar_orbit_scanner = orbit_sim.lidar_orbit_scanner:main',
-            'pointcloud_mapper = orbit_sim.pointcloud_mapper:main',
-            'impedance_controller = orbit_sim.impedance_controller:main',
-            'dual_arm_impedance_controller = orbit_sim.dual_arm_impedance_controller:main',
-            'http_ros_bridge = orbit_sim.http_ros_bridge:main',
-            'cubesat_cmg_controller = orbit_sim.cubesat_cmg_controller:main',
-            'cmg_testbed_commander = orbit_sim.cmg_testbed_commander:main',
-            'cmg_testbed_scenario = orbit_sim.cmg_testbed_scenario:main',
-            'cubesat_cmg_commander = orbit_sim.cubesat_cmg_commander:main',
-            'stereo_test_gui = orbit_sim.stereo_test_gui:main',
-            'mars_hexacopter_cmg_commander = orbit_sim.mars_hexacopter_cmg_commander:main',
         ],
     },
 )
